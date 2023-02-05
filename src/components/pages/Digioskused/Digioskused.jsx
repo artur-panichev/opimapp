9be539c406b05efdacdb5e1dd39aecfb7323d16d
ArturPanitsev <artur.panichev152@gmail.com>
2023-02-05 16:21:20 +0200

    pages content

--- src/components/pages/Digioskused/Digioskused.jsx
+++ src/components/pages/Digioskused/Digioskused.jsx
@@ -11,22 +11,28 @@ function Digioskused(){
 
             <div className="box">
                 <div>
-                    <h1 className='textCenter'>Terminid</h1>
-                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={a8ddbafe-0372-47b9-96b9-c589a9e868da}&amp;action=embedview&amp;wdPrint=0&amp;wdEmbedCode=0" width="476px" height="288px" frameborder="0"></iframe>
+                    <h1 className='textCenter'>Sõnastik</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={a8ddbafe-0372-47b9-96b9-c589a9e868da}&amp;action=embedview&amp;wdPrint=0&amp;wdEmbedCode=0" width="476px" height="288px"></iframe>
                 </div>
             </div>
 
             <div className="box">
                 <div>
                     <h1 className='textCenter'>Esitlus "Minu lemmik arvuti mäng"</h1>
-                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={842b4fa1-b1bc-42a6-955a-71b932bda823}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="476px" height="288px" frameborder="0"></iframe>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={842b4fa1-b1bc-42a6-955a-71b932bda823}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="476px" height="288px"></iframe>
                 </div>
             </div>
 
             <div className="box">
                 <div>
                     <h1 className='textCenter'>Referaat "Tervislik eluviis"</h1>
-                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={ebad36d9-e85b-435a-9776-d5af869081b1}&amp;action=embedview&amp;wdPrint=0&amp;wdEmbedCode=0" width="476px" height="288px" frameborder="0"></iframe>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={ebad36d9-e85b-435a-9776-d5af869081b1}&amp;action=embedview&amp;wdPrint=0" width="476px" height="288px"></iframe>
+                </div>
+            </div>
+            <div className="box">
+                <div className='textCenter'>
+                    <h1 className='textCenter'>Reisibüroo veebisait</h1>
+                    <a href="https://sites.google.com/view/bignametourism/" target="_blank">https://sites.google.com/view/bignametourism/</a>
                 </div>
             </div>
         </div>
--- src/components/pages/IT2/IT2.jsx
+++ src/components/pages/IT2/IT2.jsx
@@ -8,6 +8,21 @@ function IT2(){
             <h3>
                 Õpimapp | Artur Panitšev
             </h3>
+
+            <div className="box">
+                <div className="textCenter">
+                    <h1>Sõnastik 1</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={d8c13600-6a3d-43ee-8551-74dbbd2eb2e1}&amp;action=embedview&amp;wdPrint=0" height="288px"></iframe>
+                </div>
+                <div className="textCenter">
+                    <h1>Sõnastik 2</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={c69cd77e-f648-43ad-a807-e8acfb272f51}&amp;action=embedview&amp;wdPrint=0" height="288px"></iframe>
+                </div>
+                <div className="textCenter">
+                    <h1>Sõnastik 3</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={31107e50-41a3-4c10-a001-8715d571a608}&amp;action=embedview&amp;wdPrint=0" height="288px"></iframe>
+                </div>
+            </div>
         </div>
     );
 }
