ec71acc3035c41f102e5376fadc91d73f7765d47
ArturPanitsev <artur.panichev152@gmail.com>
2023-02-05 22:38:19 +0200

    added some content

--- src/components/pages/IT1/IT1.jsx
+++ src/components/pages/IT1/IT1.jsx
@@ -8,6 +8,49 @@ function IT1(){
             <h3>
                 Õpimapp | Artur Panitšev
             </h3>
+
+            <div className="box">
+                <div className="textCenter">
+                    <h1>Terminid</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={7049fc3d-fc3e-4257-9d36-bc10f4143ba0}&amp;action=embedview&amp;wdEmbedCode=1&amp;wdPrint=0" width="476px" height="288px"></iframe>
+                </div>
+            </div>
+
+            <div className="box">
+                <div className="textCenter">
+                    <h1>Euroopx Pärlid</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={d5c2227e-a6eb-43c7-8325-648580ea42a3}&amp;action=embedview&amp;wdPrint=0" width="476px" height="288px"></iframe>
+                </div>
+            </div>
+
+            <div className="box">
+                <div className="textCenter">
+                    <h1>Multimeedia tehnoloogiad</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={d3e425af-31d0-4f54-a10f-0787f4d4ce71}&amp;action=embedview&amp;wdPrint=0" width="476px" height="288px"></iframe>
+                </div>
+            </div>
+
+            <div className="box">
+                <div className="textCenter">
+                    <h1>Euroreis</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={f81382b7-0dc2-4488-a880-dc5d8abe880f}&amp;action=embedview&amp;wdPrint=0" width="476px" height="288px"></iframe>
+                </div>
+            </div>
+
+            <div className="box">
+                <div className="textCenter">
+                    <h1>Laevad</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={04e726ad-9410-42c1-aae3-249dc1894281}&amp;action=embedview&amp;wdPrint=0" width="476px" height="288px"></iframe>
+                </div>
+            </div>
+
+            <div className="box">
+                <div className="textCenter">
+                    <h1>Psühholoogia ja värv</h1>
+                    <iframe src="https://ivkhk-my.sharepoint.com/personal/artur_panitsev_ivkhk_ee/_layouts/15/Doc.aspx?sourcedoc={7f2f529a-d0d2-4cd4-aa68-0c2e7388f87f}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="476px" height="288px"></iframe>
+                </div>
+            </div>
+
         </div>
     );
 }
--- src/components/pages/Progalused/Progalused.jsx
+++ src/components/pages/Progalused/Progalused.jsx
@@ -1,4 +1,7 @@
 import '../Pages.css'
+import teachersEmails from './img/teachersEmails.jpg';
+import wordsGame from './img/wordsGame.jpg';
+import olympicAtleths from './img/olympicAtleths.jpg';
 
 function Progalused(){
     return(
@@ -8,6 +11,45 @@ function Progalused(){
             <h3>
                 Õpimapp | Artur Panitšev
             </h3>
+
+            <div className="box">
+                <div>
+                    <h1>Teachers emails parser</h1>
+                    <p>Parsib ivkhk veebisaiti ja kuvab õpetajad ja nende e-postid terminalisse.</p>
+                    <p>Allikas kood: </p>
+                    <a href="https://github.com/ArturPanitsev/teachersEmails" target="_blank">https://github.com/ArturPanitsev/teachersEmails</a>
+                </div>
+                <div>
+                    <p className='textCenter'>Tulemus:</p>
+                    <img src={teachersEmails}/> 
+                </div>
+            </div>
+
+            <div className="box">
+                <div>
+                    <h1>Words Game</h1>
+                    <p>Programm küsib tähte ja kui sisestad vale, siis 1 katse vähem, kui sõna ära arvad, siis võidad</p>
+                    <p>Allikas kood: </p>
+                    <a href="https://github.com/ArturPanitsev/wordsGame" target="_blank">https://github.com/ArturPanitsev/wordsGame</a>
+                </div>
+                <div>
+                    <p className='textCenter'>Tulemus:</p>
+                    <img src={wordsGame}/> 
+                </div>
+            </div>
+
+            <div className="box">
+                <div>
+                    <h1>Olympic athletes table</h1>
+                    <p>Veebileht olümpiasportlaste tabeliga. Otsitav ja sorteeritav</p>
+                    <p>Allikas kood: </p>
+                    <a href="https://github.com/ArturPanitsev/olympicAthletes" target="_blank">https://github.com/ArturPanitsev/olympicAthletes</a>
+                </div>
+                <div>
+                    <p className='textCenter'>Tulemus:</p>
+                    <img src={olympicAtleths}/> 
+                </div>
+            </div>
         </div>
     );
 }
